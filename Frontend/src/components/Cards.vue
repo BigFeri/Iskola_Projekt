87115515aa1cb59147f75022f2975e65cb329fae
BigFeri <ferenc0516@gmail.com>
2024-12-12 12:13:29 +0100

    Cards, paginator készülődés

---
+++ Frontend/src/components/Cards.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col" v-for="(card, index) in cards" :key="index">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <p class="card-text">
+              </p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style>
+  </style>
---
+++ Frontend/src/views/Kartyak.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Kártyák</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Cards from '@/components/Cards.vue';
+import Paginator from '@/components/Paginator.vue';
+
+export default {
+components:{
+  Cards
+},
+data(){
+  return {
+    cards: [],
+    url: 'http://localhost:8000/api'
+  }
+},
+mounted(){
+  this.GetCards();
+},
+methods: {
+  async GetCards(){
+    const url = this.url;
+    const response = await axios.get(url);
+    this.cards = response.data.data;
+    console.log(this.cards);
+    
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
