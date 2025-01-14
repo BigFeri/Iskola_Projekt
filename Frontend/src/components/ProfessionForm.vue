1c90be6dde012788b6c476b06a36b6cdeadeebc6
BigFeri <ferenc0516@gmail.com>
2025-01-14 13:14:40 +0100

    folytatás

---
+++ Frontend/src/components/ProfessionForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <p>{{ dataLine }}</p>
+    <form class="row g-4 needs-validation was-validated" novalidate>
+      <!-- Name -->
+      <div class="col-md-8">
+        <label for="sportNev" class="form-label">Sport Név:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="sportNev"
+          required
+          v-model="dataLine.name"
+        />
+        <div class="valid-feedback">&nbsp;</div>
+        <div class="invalid-feedback">Név kitöltése kötelező</div>
+      </div>
+    </form>
+  </div>
+</template>
+  
+  <script>
+import dateFormat, { masks } from "dateformat";
+export default {
+  props: ["dataLine"],
+  emits: ["saveDataLine"],
+  mounted() {
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          if (form.checkValidity()) {
+            this.onClickSubmit();
+          }
+        },
+        false
+      );
+    });
+  },
+  methods: {
+    onClickSubmit() {
+      console.log("Mentés");
+      this.$emit("saveDataLine", this.dataLine);
+    },
+  },
+  computed: {},
+};
+</script>
+  
+  <style>
+</style>
--- Frontend/src/views/Sportok.vue
+++ Frontend/src/views/Sportok.vue
@@ -1,36 +1,194 @@
 <template>
   <div>
-  <h1>Sportok</h1>
+    <h2>Sportok</h2>
+    <!-- Táblázat -->
 
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Sport Név</th>
+    <table class="table table-striped v-auto">
+      <thead>
+        <tr>
+          <!-- rename -->
+          <th scope="col">Műveletek</th>
+          <th scope="col">Sport</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="dataLine in collection"
+          :key="dataLine.id"
+          @click="onClickTr(dataLine.id)"
+          :class="{ 'table-success': selectedRowDataLineId == dataLine.id }"
+        >
+          <td class="text-nowrap">
+            <OperationsCrud
+              :dataLine="dataLine"
+              @onClickDeleteButton="onClickDeleteButton"
+              @onClickUpdate="onClickUpdate"
+              @onClickCreate="onClickCreate"
+            />
+          </td>
+          <!-- rename -->
+          <td>{{ dataLine.sportNev }}</td>
 
+        </tr>
+      </tbody>
+    </table>
+    <!-- Modal -->
+    <Modal
+      :title="title"
+      :yes="yes"
+      :no="no"
+      :size="size"
+      @yesEvent="yesEventHandler"
+    >
+      <!-- yes-no (Modal) -->
+      <div v-if="state == 'Delete'">
+        {{ messageYesNo }}
+      </div>
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
-
-</div>
-  
+      <!-- Form person -->
+      <ProfessionForm
+        v-if="state == 'Create' || state == 'Update'"
+        :dataLine="dataLine"
+        @saveDataLine="saveDataLineHandler"
+      />
+    </Modal>
+  </div>
 </template>
 
 <script>
-export default {
-
+// rename
+class DataLine {
+  constructor(id = null, sportNev = null) {
+    this.id = id;
+    this.sportNev = sportNev;
+  }
 }
+// import ProfessionForm from "@/components/ProfessionForm.vue";
+import OperationsCrud from "@/components/OperationCrud.vue";
+import * as bootstrap from "bootstrap";
+import axios from "axios";
+import { BASE_URL } from "@/helpers/baseUrls";
+
+// import uniqid from "uniqid";
+export default {
+  components: {OperationsCrud },
+  mounted() {
+    this.modal = new bootstrap.Modal("#modal", {
+      keyboard: false,
+    });
+  },
+  data() {
+    return {
+      modal: null,
+      dataLine: new DataLine(this.uniqid()),
+      selectedRowDataLineId: null,
+      messageYesNo: null,
+      state: "Read", //CRUD: Create, Read, Update, Delete
+      title: null,
+      yes: null,
+      no: null,
+      size: null,
+
+      collection: [],
+    };
+  },
+
+  mounted() {
+    this.collection = this.getCollection();
+    this.modal = new bootstrap.Modal("#modal", {
+      keyboard: false,
+    });
+  },
+  methods: {
+    //rename
+    deleteDataLineById() {
+      this.collection = this.collection.filter(
+        (p) => p.id != this.selectedRowDataLineId
+      );
+    },
+    async getCollection() {
+      const url = `${BASE_URL}/sport`;
+      this.collection = await axios.get(url);
+      this.collection = response.data.data;
+      
+    },
+    //rename
+    createDataLine() {
+      this.collection.push(this.dataLine);
+      this.state = "Read";
+    },
+    //reaname
+    updateDataLine() {
+      const index = this.collection.findIndex((p) => p.id == this.dataLine.id);
+      this.collection[index] = this.dataLine;
+      this.state = "Read";
+    },
+    yesEventHandler() {
+      if (this.state == "Delete") {
+        this.deleteDataLineById();
+        this.modal.hide();
+      }
+    },
+    onClickDeleteButton(dataLine) {
+      this.title = "Törlés";
+      this.messageYesNo = `Valóban törölniakarod? Név: ${dataLine.name}`;
+      this.yes = "Igen";
+      this.no = "Nem";
+      this.size = null;
+      this.state = "Delete";
+    },
+    onClickUpdate(dataLine) {
+      this.state = "Update";
+      this.title = "Foglalkozás módosítása";
+      this.yes = null;
+      this.no = "Mégsem";
+      this.size = "lg";
+      // this.person = person;
+      this.dataLine = { ...dataLine };
+    },
+    onClickCreate() {
+      this.title = "Új Foglalkozás létrehozása";
+      this.yes = null;
+      this.no = "Mégsem";
+      this.size = "lg";
+
+      this.state = "Create";
+      this.dataLine = new DataLine(this.uniqid());
+    },
+    onClickTr(id) {
+      this.selectedRowDataLineId = id;
+    },
+
+    saveDataLineHandler(dataLine) {
+      this.dataLine = dataLine;
+      this.modal.hide();
+      if (this.state == "Create") {
+        this.createDataLine();
+      } else if (this.state == "Update") {
+        this.updateDataLine();
+      }
+    },
+
+    uniqid(length = 10) {
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let result = "";
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+      }
+      return result;
+    },
+  },
+  computed: {
+    // collection(){
+    //   //rename
+    //   return this.professions
+    // }
+  },
+};
 </script>
 
 <style>
-
 </style>
+
